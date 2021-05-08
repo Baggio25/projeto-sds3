c130dfb7477d5c03c65d08ea2c8354d39538c410
Baggio25 <rodrigo.baggio.si@gmail.com>
2021-05-08 18:41:34 -0300

    Ajuste url heroku

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,2 @@
-export const BASE_URL = 'https://rbaggio-sds3-api.herokuapp.com';
+//export const BASE_URL = 'https://rbaggio-sds3-api.herokuapp.com';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
